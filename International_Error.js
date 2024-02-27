19169b54651447d785e6f45a77e056e8daf511b0
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 09:59:42 +0500

    Add files via upload

---
+++ International_Error.js
@@ -0,0 +1,4 @@
+"use strict";
+let newArray = ['Apple', 'Banana', 'Orange'];
+// Accessing the element at index 3 (which is out of bounds)
+console.log(newArray[3]);
---
+++ International_Error.ts
@@ -0,0 +1,8 @@
+
+let newArray: string[] = ['Apple', 'Banana', 'Orange'];
+
+
+// Accessing the element at index 3 (which is out of bounds)
+
+
+console.log(newArray[3]);
